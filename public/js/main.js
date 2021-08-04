687769e21aa147d06c005412efc0a051377e840b
Wahyu Pratama Putra <wahyumacho666@yahoo.com>
2021-08-05 00:01:58 +0800

    Added product component and change cdn load to local file

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,36 @@
+const { useState, useEffect } = React;
+
 function ProductMain() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    fetch('/api/products')
+    .then(response => response.json())
+    .then(data => {
+        setProducts(data)
+    });
+  },[])
+
+  const ListProduct = ()=>{
+    return products.map((product, index) => 
+      <div className="col-3" key={index}>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">${product.price}</h5>
+            <div className="card-text" style={{fontSize:product.size}}>{product.face}</div>
+            <div>{product.date}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-        test
+    <div className="row">
+        <ListProduct/>
     </div>
   );
 }
+
+
 ReactDOM.render(<ProductMain />, document.getElementById('app-product'))
